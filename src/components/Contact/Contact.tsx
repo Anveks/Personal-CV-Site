9179d40b7af0e6fb42e59551c721da3a81418c5d
Anveks <vekselmana@gmail.com>
2023-07-23 15:48:56 +0300

    emailjs + contact me form + whatsapp + email added

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,10 +1,56 @@
 import "./Contact.css";
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 function Contact(): JSX.Element {
+    const form: any = useRef()
+    const rows = 7
+
+    const sendEmail = (e: any) => {
+        e.preventDefault();
+
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
         <section id="contact">
-            contact
-        </section>
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className="container contact_container">
+                {/* EMAIL and WHATSAPP */}
+                <div className="contact_options">
+                    <div className="contact_option">
+                        <AlternateEmailIcon className="contact_option-icon" />
+                        <h4>Email</h4>
+                        <h5>example@gmail.com</h5>
+                        <a href="mailto:example@gmail.com">Send a Message</a>
+                    </div>
+
+                    <div className="contact_option">
+                        <WhatsAppIcon className="contact_option-icon" />
+                        <h4>WhatsApp</h4>
+                        <h5>000-000-00-00</h5>
+                        <a href="https://api.whatsapp.com/send?phone=00000000" target="_blank">Send a  Message</a>
+                    </div>
+                </div>
+
+                {/* CONTACT FORM */}
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" placeholder="John Doe" required />
+                    <input type="email" placeholder="example@gmail.com" required />
+                    <textarea name="message" rows={rows} placeholder="Hello! I am writing to..." required></textarea>
+                    <button type="submit" className="button button-primary">Send Message</button>
+                </form>
+            </div>
+        </section >
     );
 }
 
--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,4 +1,5 @@
 import "./Portfolio.css";
+import data from '../../utils/projects-data.json'
 import ex_2 from '../../assets/example_1.jpg'
 import ex_1 from '../../assets/example_2.png'
 import ex_3 from '../../assets/example_3.jpg'
@@ -10,6 +11,16 @@ function Portfolio(): JSX.Element {
             <h2>Portfolio</h2>
 
             <div className="container portfolio_container">
+                {/* {
+                    data.map((e) =>
+                        <div key={e.id} className="portfolio_item">
+                            <div className="portfolio_item-image">
+                                <img src={e.img} alt="preview-image" />
+                            </div>
+                            <h3>{e.title}</h3>
+                            <a href={e.github} className="button" target="_blank">Github</a>
+                        </div>)
+                } */}
                 <div className="portfolio_item">
                     <div className="portfolio_item-image">
                         <img src={ex_1} alt="crypto-site" />
